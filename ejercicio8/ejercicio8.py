15dd01f353a0aeab527499abbd86952247f31f44
tarazona2006 <andressuarez@colegioguanenta.edu.co>
2023-03-09 15:33:08 -0500

    se creo el diagrama de flujo y se agrego al READAME

---
+++ ejercicio8/ejercicio8.py
@@ -0,0 +1,13 @@
+# input
+
+x=int(input("si: "))
+y=int(input("es multiplo de: "))
+
+# processing
+
+if x % y==0:
+    msj="el numero" ,x, "es multiplo de" ,y,
+else:
+    msj="no son multiplos"
+
+print(msj)
